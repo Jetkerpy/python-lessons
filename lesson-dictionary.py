55e747bcd6178c16f04ab1775c1f55bda528bd3a
jetkerGit <berdibaevichjetker@gmail.com>
2022-06-26 00:45:56 +0500

    Create lesson-dictionary.py

---
+++ lesson-dictionary.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 06:27:54 2022
+
+@author: Jetker
+"""
+
+# class Insan:
+#     """Insan haqida"""
+#     def __init__(self, ism, family, tjil, id_nomer):
+#         self.name = ism.title()
+#         self.family = family.title()
+#         self.year = tjil
+#         self.id = id_nomer.upper()
+#         self.kurs = 1
+        
+        
+#     def get_info(self):
+#         info = f"{self.family} {self.name}. Ol {self.year}-jili tuwilg'an,"
+#         info += f"id nomeri: {self.id} ha'm {self.kurs}-basqish kursda oqiydi."
+#         return info
+    
+#     def update_k(self):
+#         self.kurs += 1
+        
+#     def __repr__(self):
+#         return f"{self.name} {self.family}"
+    
+
+
+# class Student(Insan):
+#     def __init__(self, ism, family, tjil, id_nomer,passport):
+#         super().__init__(ism, family, tjil, id_nomer)
+#         self.passport = passport.upper()
+#         self.students = []
+#         self.sani = 0
+        
+    
+#     def add_student(self,*student):
+#         self.students.append(student)
+        
+
+
+# insan1 = Insan('nokisbay', 'kenesbaev', 1996, 'nk99999')
+# insan2 = Insan('aynura', 'kenesbaeva', 2002, 'ay99999')
+# insan3 = Insan('ayzada', 'allamuratova', 1996, 'aa99999')
+        
+# student1 = Student('nokisbay', 'kenesbaev', 1996, 'nk99999', 'nk0000')
+
+
+
+# sena = 10000
+# tea = False
+# salat = True
+
+# if tea and salat:
+#     sena1 = sena + 10000
+    
+# elif tea or salat:
+#     sena1 = sena + 5000
+
+# print(f"Jami {sena1} sum boldi")
+
+# sena = 15000
+# tea = 1
+# bread = 0
+# may = 1
+# qant = 1
+# salat = 1
+# if tea:
+#     print("Tea aldi")
+#     sena = sena + 2000
+
+# if bread:
+#     print("Nan aldi")
+    
+#     sena = sena + 6000
+
+# if may:
+#     print("May aldi")
+#     sena = sena + 11000
+
+# if qant:
+#     print("Qant aldi")
+#     sena = sena + 3000
+
+# if salat:
+#     print("Salat aldi")
+#     sena = sena + 7000
+
+# print(f"Jami {sena} sum")
+
+
+
+# san = int(input("Qalegen sandi kiritin>>> "))
+
+# for i in range(2,11):
+#     if san %i == 0:
+#         print(f"{san} {i}g'a qaldiqsiz bolinedi" )
+
+
+
+
+
+
+
+
+
+
+
+
+
